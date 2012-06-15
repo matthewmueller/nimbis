fad25f96fd4329b391f0ab3f986639eef346108b
Matt Mueller <mattmuelle@gmail.com>
2012-06-14 18:16:25 -0700

    replaced the models

--- client/models/user.js
+++ client/models/user.js
@@ -1,1 +1,55 @@
-module.exports = "hello from user!";
+var Backbone = require('backbone');
+
+/*
+  Expose the `User` model
+*/
+var User = module.exports = Backbone.Model.extend();
+
+/*
+  Model Name
+*/
+User.prototype.name = 'User';
+
+/*
+  Augment get functionality to work with functions
+*/
+User.prototype.get = function(attr) {
+  if (typeof this.attributes[attr] === 'function') {
+    return this.attributes[attr].call(this);
+  } else
+    return Backbone.Model.prototype.get.call(this, attr);
+};
+
+/*
+  Set the defaults
+*/
+User.prototype.defaults = {
+  firstName : "",
+  lastName : ""
+};
+
+/*
+  Use a recursive `toJSON`
+*/
+User.prototype.toJSON = User.prototype.recursiveToJSON;
+
+/*
+  Attribute functions
+*/
+// User.prototype.attributes = {
+//   name : function() {
+//     console.log('lol');
+//     return [this.get('firstName'), this.get('lastName')].join(' ');
+//   }
+// };
+
+/*
+  Initialize the model
+*/
+User.prototype.initialize = function() {
+  var name = this.get('firstName') + ' ' + this.get('lastName');
+  this.set('fullName', name);
+
+  var groups = new App.Collections.Groups(this.get('groups'));
+  this.set('groups', groups);
+};
