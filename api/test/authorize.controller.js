fcaf7100eba4d4bcd63a13f02fbe9390a356032f
Matt Mueller <mattmuelle@gmail.com>
2012-09-12 21:11:49 -0700

    updated authorize controller

--- api/test/authorize.controller.js
+++ api/test/authorize.controller.js
@@ -1,34 +1,56 @@
+/**
+ * Module dependencies
+ */
+
 var expect = require('expect.js'),
     request = require('supertest'),
     client = require('../support/client'),
     User = require('../models/user'),
-    app = require('../app.js');
+    api = require('../api.js');
 
-var userAttributes = {
+/**
+ * User attributes
+ */
+
+var attrs = {
   name : 'Matt Mueller',
   email : 'test@test.com',
   password : 'test'
 };
 
+/**
+ * Test the `/authorize` route
+ */
+
 describe('Authorize controller', function() {
   var user;
 
+  /**
+   * Create a user
+   */
+  
   before(function(done) {
-    User.create(userAttributes, function(err, model) {
+    User.create(attrs, function(err, model) {
       if(err) return done(err);
       user = model;
       done();
     });
   });
 
+  /**
+   * Destroy a user
+   */
+  
   after(function(done) {
-    user.destroy(function(err) {
-      done(err);
-    });
+    user.destroy(done);
   });
 
+  /**
+   * POST /authorize
+   */
+  
   it('should set the proper cookie, when you authorize correctly', function(done) {
-    request(app)
+    request(api)
       .post('/authorize')
       .set('Content-Type', 'application/json')
       .send({ email : 'test@test.com', password : 'test' })
@@ -36,13 +58,10 @@ describe('Authorize controller', function() {
       .expect(200)
       .end(function(err, res) {
         if(err) return done(err);
-
         expect(res.body.token).to.be.ok();
         expect(res.body.token).to.match(/[\w\d]{0,20}/);
-
         done();
       });
-
   });
 
 });
